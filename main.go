2d483adf724644dabb66221cd6ccf9da8b0c6960
paedayz <pae16phasit@gmail.com>
2021-04-29 22:42:19 +0700

    parseRtmParams

--- main.go
+++ main.go
@@ -140,9 +140,25 @@ func parseRtcParams(c *gin.Context) (channelName, tokentype, uidStr string, role
 	return channelName, tokentype, uidStr, role, expireTimestamp, err
 }
 
-// func parseRtmParams(c *gin.Context) (uidStr string, expireTimestamp uint32, err error) {
+func parseRtmParams(c *gin.Context) (uidStr string, expireTimestamp uint32, err error) {
+	// get param values
+	uidStr = c.Param("uid")
+	expireTime := c.DefaultQuery("expiry", "3600")
 
-// }
+	expireTime64, parseErr := strconv.ParseUint(expireTime, 10, 64)
+	if parseErr != nil {
+		// if string conversion fails return an error
+		err = fmt.Errorf("failed to parse expireTime: %s, causing error: %s", expireTime, parseErr)
+	}
+
+	// set timestamps
+	expireTimeInSeconds := uint32(expireTime64)
+	currentTimestamp := uint32(time.Now().UTC().Unix())
+	expireTimestamp = currentTimestamp + expireTimeInSeconds
+
+	// check if string conversion fails
+	return uidStr, expireTimestamp, err
+}
 
 func generateRtcToken(channelName, uidStr, tokentype string, role rtctokenbuilder.Role, expireTimestamp uint32) (rtcToken string, err error) {
 	if tokentype == "userAccount" {
